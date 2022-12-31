61f9ef3f3226a87b22b255954d8e7cc39ed24404
dek <dakoner@gmail.com>
2022-12-31 10:00:24 -0800

    add spin viewer

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     name="video_sender",
     version="1.0",
     packages=['video_sender', 'video_sender.pyspin_camera', 'video_sender.uvc_camera'],
-    scripts=['scripts/video-sender', 'scripts/video-viewer']
+    scripts=['scripts/video-sender', 'scripts/gige-video-viewer', 'scripts/spin-video-viewer']
 )
